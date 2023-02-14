bbdeacb2af2940c3633c9dc80a5a450413f64dd6
Phoebe Leung <phoebeleungtc@gmail.com>
2023-02-14 22:22:00 +0000

    Finished Contact Desktop and Mobile Layout

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,34 @@
+import pig from '../images/pig.png';
+import speech from '../images/speech.png'
+
 const Contact = () => {
     return <>
-    <div className="frame">
-    <h1>Contact</h1><h1>Contact</h1><h1>Contact</h1><h1>Contact</h1>
-    </div> 
+    <div className="frame-contact">
+            <div className="email-contact">
+                <a href="mailto:phoebeleungtc@gmail.com" className="email-text-contact">SEND ME A MESSAGE !</a>
+                <div className="speech-contact">
+                    <img src={speech} alt="speech" className="speech-img1-contact"></img>
+                    <img src={speech} alt="speech" className="speech-img2-contact"></img>
+                </div>
+                <div className="pig-contact">
+                    <img src={pig} alt="pig" className="pig-img1-contact"></img>
+                    <img src={pig} alt="pig" className="pig-img2-contact"></img>
+                    <img src={pig} alt="pig" className="pig-img3-contact"></img>
+                </div>
+            </div>
+        
+            <div className="footer-contact">
+                <div className="socialmedia-contact">
+                    <a href="https://www.instagram.com/phoebeltc/" target="_blank" className="smlink-contact">Instagram</a>
+                    <a href="https://github.com/phoebeltc" target="_blank" className="smlink-contact">GitHub</a>
+                    <a href="https://www.linkedin.com/in/tsz-ching-leung-6b8972bb/" target="_blank" className="smlink-contact">Linkedin</a>
+                    <a target="_blank" className="smlink-contact">CV</a>
+                </div>
+                <div>
+                    <p>@Phoebe Leung 2023</p>
+                </div>
+            </div>
+        </div>
     </>
 }
 

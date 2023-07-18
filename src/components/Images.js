5dfbacb697b29b19d5de6718aad7ab82763487f6
Phoebe Leung <phoebeleungtc@gmail.com>
2023-07-18 23:04:50 +0100

    Start building slider

---
+++ src/components/Images.js
@@ -0,0 +1,8 @@
+import image1 from "../images/PoliLearn.jpg";
+import image2 from "../images/DRD.jpg";
+import image3 from "../images/Doglivery.jpg";
+import image4 from "../images/AngelFace.jpg";
+import image5 from "../images/Menya.jpg";
+
+export default [image1, image2, image3, image4, image5];
+
--- src/pages/Works.js
+++ src/pages/Works.js
@@ -1,40 +1,30 @@
-import Work from '../components/Work'
-import githubIcon from '../images/github-mark.svg';
-import arrow from '../images/arrow-right.svg';
 import { useState, useEffect, useRef } from 'react';
 import { motion, useInView } from 'framer-motion';
-
-// import PoliLearn from '../images/PoliLearn.png';
-// import DRD from '../images/DRD.png';
-// import Doglivery from '../images/Doglivery.png';
-// import AngelFace from '../images/AngelFace1.png';
-// import Menya from '../images/Menya.png';
-
+import images from "../components/Images"; 
 
 const Works = () => {
-
-
-    const [expanded, setExpanded] = useState('panel1');
-
-    const handleClick = (panel) => e => {
-        setExpanded(expanded === panel? false : panel);
-    }
-
-    const detail1 = useRef(null);
-
-
-
     return <>
-    <div className="frame-works" ref={detail1}> 
+    <div className="frame-works"> 
         <div className='wrapper-work'>
-            <h6>Check out of my projects!</h6>
-            <div className='works-listing'>
+            <p>Check out of my projects! XOXO</p>
+            {/* <div className='works-listing'>
                 <a href={process.env.PUBLIC_URL + "/images/Works-PoliLearn.pdf"} target="_blank"><img src={process.env.PUBLIC_URL + "/images/PoliLearn.png"} alt="PoliLearn" className='responsive'></img></a>
                 <a href={process.env.PUBLIC_URL + "/images/Works-DPD.pdf"} target="_blank"><img src={process.env.PUBLIC_URL + "/images/DRD.png"} alt="DRD" className='responsive' ></img></a>
                 <a href={process.env.PUBLIC_URL + "/images/Works-PoliLearn.pdf"} target="_blank"><img src={process.env.PUBLIC_URL + "/images/Doglivery.png"} alt="Doglivery" className='responsive'></img></a>
                 <a href={"https://www.angelface.com.hk/"} target="_blank"><img src={process.env.PUBLIC_URL + "/images/AngelFace1.png"} alt="AngelFace" className='responsive'></img></a>
                 <a href={process.env.PUBLIC_URL + "/images/Works-PoliLearn.pdf"} target="_blank"><img src={process.env.PUBLIC_URL + "/images/Menya.png"} alt="Menya" className='responsive'></img></a>
-            </div>
+            </div> */}
+            <motion.div className='carousel'>
+                <motion.div className='inner-carousel'>
+                    {images.map(image => {
+                        return(
+                            <motion.div className='item'>
+                                <img src={image} alt="" />
+                            </motion.div>
+                        );
+                    })}
+                </motion.div>
+            </motion.div>
             
         </div>
     </div> 
